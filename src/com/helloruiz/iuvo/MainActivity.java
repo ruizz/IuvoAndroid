87430c5ae4bf5a166898c00ec8d444bff0e24538
Ruiz <ruiz.akpan@gmail.com>
2013-07-13 16:38:10 -0500

    Not sure. There was a modified file on my laptop.

--- src/com/helloruiz/iuvo/MainActivity.java
+++ src/com/helloruiz/iuvo/MainActivity.java
@@ -25,7 +25,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.TextView;
